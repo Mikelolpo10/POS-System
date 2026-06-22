9d9b03988542d3a878ccef107e8c162011c5e016
Mikelolpo10 <xzvmike@gmail.com>
2026-06-22 10:01:47 +0700

    Style: changed all font to inter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import { Routes, Route } from 'react-router'
 import MainLayout from './components/MainLayout'
 import Mainpage from './pages/Mainpage'
